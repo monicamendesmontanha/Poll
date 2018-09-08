f0e952ac33ddb5194e3eb81de60aae03d76041bc
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-09-08 18:19:37 +1000

    count only valid candidates

--- src/pool/Poll.java
+++ src/pool/Poll.java
@@ -5,13 +5,17 @@ import java.util.List;
 public class Poll {
 
     private int votes;
+    private List<Integer> candidates;
 
     public Poll(List<Integer> candidates) {
+        this.candidates = candidates;
         this.votes = 0;
     }
 
     public void addVote(Integer candidate) {
-        this.votes = this.votes+1;
+        if (this.candidates.contains(candidate)) {
+            this.votes = this.votes + 1;
+        }
     }
 
     public Integer totalVotes() {
--- test/pool/PoolTest.java
+++ test/pool/PoolTest.java
@@ -26,4 +26,19 @@ public class PoolTest {
         assertEquals(new Integer(4), poll.totalVotes());
     }
 
+    @Test
+    public void shouldCountOnlyValidCandidates() {
+
+        List<Integer> candidates = new ArrayList<>();
+        candidates.add(1);
+        candidates.add(2);
+
+        Poll poll = new Poll(candidates);
+        poll.addVote(1);
+        poll.addVote(2);
+        poll.addVote(3);
+
+        assertEquals(new Integer(2), poll.totalVotes());
+    }
+
 }
